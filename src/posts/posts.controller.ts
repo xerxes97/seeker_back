5aa196eeeb33141cc509cc187e0f8a18f5e17966
usuario <ccquevedos>
2026-05-06 22:37:21 -0500

    feat(profile): get userId

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { PipelineService } from './pipeline/pipeline.service';
 import { ProcessPostsDto } from './dto/process-posts.dto';
+import { GetUserId } from 'src/auth/decorators/get-user.decorator';
 
 @Controller('posts')
 export class PostsController {
@@ -9,12 +10,13 @@ export class PostsController {
   constructor(private readonly pipelineService: PipelineService) {}
 
   @Post()
-  async create(@Body() createPostsDto: ProcessPostsDto) {
+  async create(@Body() createPostsDto: ProcessPostsDto, @GetUserId() userId: string) {
     createPostsDto.posts.forEach((post) => {
       this.logger.log(`Received post: ${post.postId}`);
     });
     const results = await this.pipelineService.processPosts(
       createPostsDto.posts,
+      userId,
     );
     return { accepted: true, results };
   }
